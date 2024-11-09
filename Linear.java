ce65ab1af9cd1ae5569917b48e71d0207ddd7404
Raveena kirar <raveenakirar2@gmail.com>
2024-11-09 19:29:34 +0530

    Add files via upload

---
+++ Linear.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Linear{
+    public static int linearSearch(int nums[] , int key){
+        for (int  i=0;i<=nums.length;i++){
+            if ( nums[i]==key){
+        System.out.print("key is find"+nums[i]);
+              
+        }
+    } 
+       return -1;
+}
+ public static void main(String args[]){
+    int nums[]={1,5,7,3,9};
+    int  key=9;
+  
+    }
+ } 
